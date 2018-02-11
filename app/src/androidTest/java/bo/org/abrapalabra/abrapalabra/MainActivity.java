236f709399dca45382a5078c892baef7f9c0a4b5
xcorp7 <scorp7@gmail.com>
2018-02-10 21:12:59 -0400

    App 1 Abra Palabra

--- app/src/androidTest/java/bo/org/abrapalabra/abrapalabra/MainActivity.java
+++ app/src/androidTest/java/bo/org/abrapalabra/abrapalabra/MainActivity.java
@@ -172,7 +172,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);//
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
